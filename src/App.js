cb83638f1339bef46adf68fd2360afd29579431c
“Arunthamizh” <arunthamizh.azhagiri@twilightitsolutions.com>
2023-12-05 19:55:59 +0530

    3rd video Component update in action

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
-import React from 'react';
+import React, { useState } from 'react';
+import Button from './components/UI/Button/Button';
 
 import './App.css';
 
 function App() {
+
+const [showParagraph, setShowParagraph] = useState(false);
+
   return (
     <div className="app">
       <h1>Hi there!</h1>
+      {showParagraph && <p>This is new!</p>}
+      <Button onClick={() => setShowParagraph(!showParagraph)}>Toggle Paragraph</Button>
     </div>
   );
 }
